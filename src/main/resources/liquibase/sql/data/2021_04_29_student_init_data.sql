1747340342e5d38cd24cbbba35f62924f6035bec
Ibrahim Mohsen <i.Mohsen-c@tamkeentech.sa>
2021-04-30 22:16:43 +0300

    Add database & liquibase configuration with student table and init data

---
+++ src/main/resources/liquibase/sql/data/2021_04_29_student_init_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO student (first_name, last_name, email)
+VALUES
+       ( 'John','Smith', 'john_smith@gmail.com' ),
+       ( 'Raj','Dave', 'raj_dave@gmail.com' ),
+       ( 'Ibrahim','Mohsen', 'ibrahim_moh@gmail.com' );
